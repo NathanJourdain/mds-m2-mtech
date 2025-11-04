a9f0a82e7a7a3e76c74a481ca2a83178a6d52973
Nathan Jourdain <nathan.jourdain50700@gmail.com>
2025-11-04 15:41:38 +0100

    Test #deploy avec erreur

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -16,6 +16,6 @@ final class CalculatorTest extends TestCase
     // Test volontairement erroné pour l'activité "Erreur et correction"
     public function testAdditionNegativeNumbers(): void
     {
-        $this->assertSame(-5, Calculator::add(-2, -3)); // échouera (résultat attendu: -5)
+        $this->assertSame(-4, Calculator::add(-2, -3)); // échouera (résultat attendu: -5)
     }
 }
