c17b6939d4d49c55c58c51417fe6d6d982d2b161
Nathan Jourdain <nathan.jourdain50700@gmail.com>
2025-11-04 16:24:33 +0100

    #deploy success

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -10,7 +10,6 @@ final class CalculatorTest extends TestCase
 {
     public function testAdditionPositiveNumbers(): void
     {
-        throw new \Exception("Test erroné volontairement pour l'activité 'Erreur et correction'"); // Lancer une exception pour simuler une erreur
         $this->assertSame(5, Calculator::add(2, 3));
     }
 
